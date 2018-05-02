ed103172aec9dccb7f1f9886b12ee2d8d1072cfa
ottagit <ottacc66@gmail.com>
2018-05-02 10:24:58 +0300

    Depot scaffold

---
+++ db/seeds.rb
@@ -0,0 +1,21 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create!(title: 'Take My Money',
+                description:
+                %{<p>
+                  <em>Native Apps, Multiple Platforms</em>
+                  Answer the question “Can we build this for ALL the devices?” with a
+                  resounding YES. This book will help you get there with a real-world
+                  introduction to seven platforms, whether you’re new to mobile or an
+                  experienced developer needing to expand your options. Plus, you’ll find
+                  out which cross-platform solution makes the most sense for your needs.
+                </p>},
+                image_url: 'eating.jpg',
+                price: 12.49)
+
