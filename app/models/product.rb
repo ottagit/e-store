fbd78bcdfd8aed684a8637a3c14d4eaf789e657e
ottagit <ottacc66@gmail.com>
2018-05-04 21:52:39 +0300

    specify inverse relations for line_item
    Also, prevent deletion of products that
    are referenced by line items

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,20 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   validates :title, uniqueness: true, length: { minimum: 10 }
   validates :image_url, allow_blank: true, format: { with: %r{\.(gif|jpg|jpeg|png)\Z}i,
     message: "Must be a URL for GIF, JPG, JPEG or PNG image." }
+
+  private
+
+    def ensure_not_referenced_by_any_line_item
+      unless line_items.empty?
+        errors.add(:base, 'Line items present')
+        throw :abort
+      end
+    end
 end
