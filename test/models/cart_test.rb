2d6632fac5c64107f014027eef815f775fd69e73
ottagit <ottacc66@gmail.com>
2018-06-09 13:34:10 +0300

    test addition of unique and duplicate products
    Assert how many products should be in the cart
    in each instance.

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,28 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @cart = Cart.create
+    @book_one = products(:one)
+    @book_two = products(:two)
+    @week_book = products(:week)
+  end
+
+  test "add unique products" do
+
+    @cart.add_product(@book_one).save!
+    @cart.add_product(@book_two).save!
+    assert_equal 2, @cart.line_items.size
+    assert_equal @book_one.price + @book_two.price, @cart.total_price
+  end
+
+  test "add duplicate product" do
+    @cart.add_product(@week_book).save!
+    @cart.add_product(@week_book).save!
+    assert_equal 2 * @week_book.price, @cart.total_price
+    assert_equal 1, @cart.line_items.size
+    assert_equal 2, @cart.line_items[0].quantity
+  end
+
 end
