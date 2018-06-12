ba724b65c1a72751f1507c91a2cf9ad23d2bb0ca
ottagit <ottacc66@gmail.com>
2018-06-12 12:39:30 +0300

    hide checkout button on checkout screen display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,11 @@ module ApplicationHelper
       render record
     end
   end
+
+  def hidden_div_if(condition, attributes = {}, &block)
+    if condition
+      attributes['style'] = "display: none"
+    end
+    content_tag("div", attributes, &block)
+  end
 end
