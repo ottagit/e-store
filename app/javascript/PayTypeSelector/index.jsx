6760ac3a300f888eef01d091c16c5a9f6f1de389
ottagit <ottacc66@gmail.com>
2018-06-20 17:32:17 +0300

    render custom component based on selected pay type

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 
+import NoPayType from './NoPayType';
+import CreditCardPayType from './CreditCardPayType';
+import CheckPayType from './CheckPayType';
+import PurchaseOrderPayType from './PurchaseOrderPayType';
+
 class PayTypeSelector extends React.Component {
   constructor(props) {
     super(props);
@@ -12,15 +17,31 @@ class PayTypeSelector extends React.Component {
   }
 
   render() {
+    let PayTypeCustomComponent = NoPayType;
+
+    if (this.state.selectedPayType == "Credit card") {
+      PayTypeCustomComponent = CreditCardPayType;
+    }
+    else if (this.state.selectedPayType == "Check") {
+      PayTypeCustomComponent = CheckPayType;
+    }
+    else if (this.state.selectedPayType == "Purchase order") {
+      PayTypeCustomComponent = PurchaseOrderPayType;
+    }
+  
     return (
-        <div className="field">
-          <label htmlFor="order_pay_type">Pay type</label>
-          <select id="pay_type" onChange={this.onPayTypeSelected} name="order[pay_type]">
-            <option value="">Select a payment method</option>
-            <option value="Check">Check</option>
-            <option value="Credit card">Credit card</option>
-            <option value="Purchase order">Purchase order</option>
-          </select>
+        <div>
+          <div className="field">
+            <label htmlFor="order_pay_type">Pay type</label>
+            <select id="pay_type" onChange={this.onPayTypeSelected} name="order[pay_type]">
+              <option value="">Select a payment method</option>
+              <option value="Check">Check</option>
+              <option value="Credit card">Credit card</option>
+              <option value="Purchase order">Purchase order</option>
+            </select>
+          </div>
+
+          <PayTypeCustomComponent />
         </div>
         );
   }
