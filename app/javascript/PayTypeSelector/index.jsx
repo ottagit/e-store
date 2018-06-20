e68fd54c0a0eeb40fec24243e86c722a62c06d32
ottagit <ottacc66@gmail.com>
2018-06-20 16:13:35 +0300

    get selected pay type into component's state

--- app/javascript/PayTypeSelector/index.jsx
+++ app/javascript/PayTypeSelector/index.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
 class PayTypeSelector extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onPayTypeSelected = this.onPayTypeSelected.bind(this);
+    this.state = { selectedPayType: null };
+  }
+
+  onPayTypeSelected(event) {
+    this.setState({ selectedPayType: event.target.value });
+  }
+
   render() {
     return (
         <div className="field">
           <label htmlFor="order_pay_type">Pay type</label>
-          <select id="pay_type" name="order[pay_type]">
+          <select id="pay_type" onChange={this.onPayTypeSelected} name="order[pay_type]">
             <option value="">Select a payment method</option>
             <option value="Check">Check</option>
             <option value="Credit card">Credit card</option>
