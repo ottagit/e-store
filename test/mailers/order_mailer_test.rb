8477ccba8e7fb28864079cb7159b675067c5cebe
ottagit <ottacc66@gmail.com>
2018-06-24 17:28:07 +0300

    test order email templates

--- test/mailers/order_mailer_test.rb
+++ test/mailers/order_mailer_test.rb
@@ -2,19 +2,19 @@ require 'test_helper'
 
 class OrderMailerTest < ActionMailer::TestCase
   test "received" do
-    mail = OrderMailer.received
-    assert_equal "Received", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.received(orders(:one))
+    assert_equal "My Favorite Store Order Confirmation", mail.subject
+    assert_equal ["email@example.com"], mail.to
+    assert_equal ["otta.chris@me.com"], mail.from
+    assert_match /1 x The 4-Hour Work Week/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderMailer.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderMailer.shipped(orders(:one))
+    assert_equal "Your Favorite Store Order Shipped", mail.subject
+    assert_equal ["email@example.com"], mail.to
+    assert_equal ["otta.chris@me.com"], mail.from
+    assert_match /<td[^>]*>1<\/td>\s*<td>The 4-Hour Work Week<\/td>/, mail.body.encoded
   end
 
 end
