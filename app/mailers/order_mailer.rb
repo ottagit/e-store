538ae69f397fa3d400a221c299654eb8eb2af2ae
ottagit <ottacc66@gmail.com>
2018-06-24 15:11:45 +0300

    generate mailer for received and shipped orders
    Manually create the line item partial template
    to render the qunaity and title of product. Update
    the recieved method to take in order as an argument.

---
+++ app/mailers/order_mailer.rb
@@ -0,0 +1,26 @@
+class OrderMailer < ApplicationMailer
+
+  default from: 'Chris Otta <otta.chris@me.com>'
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.received.subject
+  #
+  def received(order)
+    @order = order
+
+    mail to: order.email, subject: "My Favorite Store Order Confirmation"
+  end
+
+  # Subject can be set in your I18n file at config/locales/en.yml
+  # with the following lookup:
+  #
+  #   en.order_mailer.shipped.subject
+  #
+  def shipped
+    @greeting = "Hi"
+
+    mail to: "to@example.org"
+  end
+end
