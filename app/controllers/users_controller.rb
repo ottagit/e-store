58aef89f1e74a9637ba1a6495b4e660a8b5623d2
ottagit <ottacc66@gmail.com>
2018-08-15 23:23:48 +0300

    require and validate password b4 user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,13 +40,15 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to users_url, notice: "User #{@user.name} was successfully updated." }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+    unless @user.try(:authenticate, session[:password])
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to users_url, notice: "User #{@user.name} was successfully updated." }
+          format.json { render :show, status: :ok, location: @user }
+        else
+          format.html { render :edit }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
